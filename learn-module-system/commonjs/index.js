60eec44cdecacd3c8eaa16f2809ac180a36dc7b8
Joon <pss21242124@gmail.com>
2024-12-22 20:18:54 +0900

    24.12.22

---
+++ learn-module-system/commonjs/index.js
@@ -0,0 +1,13 @@
+const { perfectScore, sum, avg, subtract } = require('./math');
+
+console.log(perfectScore);
+console.log(sum(80, 10));
+console.log(avg(80, 90));
+console.log(subtract(80, 90));
+
+// const math = require('./math');
+
+// console.log(math.perfectScore);
+// console.log(math.sum(80, 10));
+// console.log(math.avg(80, 90));
+// console.log(math.subtract(80, 90));
---
+++ learn-module-system/module/index.js
@@ -0,0 +1,8 @@
+// import { perfectScore, sum, avg } from './math.js';
+import math from './math.js';
+// import * as math from './math.js';
+
+console.log(math.perfectScore);
+console.log(math.sum(80, 10));
+console.log(math.avg(80, 90));
+console.log(math.subtract(80, 90));
